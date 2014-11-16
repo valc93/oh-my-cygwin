05afcce93ec215a04e18901bbe17edb4c120da90
Victor A. Lacayo <valc93@gmail.com>
2014-11-15 21:00:39 -0500

    removed {APT-CYG}

--- oh-my-cygwin.sh
+++ oh-my-cygwin.sh
@@ -2,15 +2,14 @@
 set -e
 
 SIMPLE_BACKUP_SUFFIX=".orig"
-APT_CYG="$(mktemp /tmp/apt-cyg.XXXXXXXX)"
 
 # install apt-cyg
-wget --no-check-certificate "rawgit.com/transcode-open/apt-cyg/master/apt-cyg" -O "${APT_CYG}"
-chmod +x "${APT_CYG}"
+wget --no-check-certificate "rawgit.com/transcode-open/apt-cyg/master/apt-cyg"
+chmod +x apt-cyg
+mv apt-cyg /usr/local/bin/
 
 # install some stuff like vim and git
-"${APT_CYG}" install zsh mintty vim curl git openssh
-
+apt-cyg install zsh mintty vim curl git openssh
 
 # install OH MY ZSH
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh; if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then cp ~/.zshrc ~/.zshrc.orig; rm ~/.zshrc; fi 
@@ -27,9 +26,6 @@ if [ ! -f ~/.vimrc ] && [ -n "${VIMRC_EXAMPLE}" ]
 then
   install "${VIMRC_EXAMPLE}" ~/.vimrc
 fi
-
-# install apt-cyg
-install --backup "${APT_CYG}" /usr/local/bin/apt-cyg
 
 # setting up zsh as default
 sed -i "s/$USER\:\/bin\/bash/$USER\:\/bin\/zsh/g" /etc/passwd
